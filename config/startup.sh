e930a9acc6491c67b8c3223dd766a230ee2bdbb9
cludden <chris.ludden@gmail.com>
2016-08-27 11:23:49 -0600

    adds conditional logic around copying ssl stuff from s3

--- config/startup.sh
+++ config/startup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # copy ssl certificates
-aws s3 cp "s3://${S3_BUCKET}/${CERT_S3_PATH}" "${CERT_DEST_PATH}" --region "$S3_REGION"
-aws s3 cp "s3://${S3_BUCKET}/${KEY_S3_PATH}" "${KEY_DEST_PATH}" --region "$S3_REGION"
-aws s3 cp "s3://${S3_BUCKET}/${CA_S3_PATH}" "${CA_DEST_PATH}" --region "$S3_REGION"
+if [ -z "$CERT_S3_PATH" ]; then
+    aws s3 cp "s3://${S3_BUCKET}/${CERT_S3_PATH}" "${CERT_DEST_PATH}" --region "$S3_REGION"
+fi
+if [ -z "$KEY_S3_PATH" ]; then
+    aws s3 cp "s3://${S3_BUCKET}/${KEY_S3_PATH}" "${KEY_DEST_PATH}" --region "$S3_REGION"
+fi
+if [ -z "$CA_S3_PATH" ]; then
+    aws s3 cp "s3://${S3_BUCKET}/${CA_S3_PATH}" "${CA_DEST_PATH}" --region "$S3_REGION"
+fi
 
 # get configuration from DynamoDB
 aws dynamodb get-item --region "$DYNAMO_REGION" --table-name "$DYNAMO_TABLE" --key="$DYNAMO_KEY" | \
